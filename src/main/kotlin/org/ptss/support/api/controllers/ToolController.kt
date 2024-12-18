9b3db061c3f1fe771b01c1d230479677a59d474f
DaanTol <686379@student.inholland.nl>
2024-12-18 23:13:58 +0100

    Made env.examples for postgreSQL

--- src/main/kotlin/org/ptss/support/api/controllers/ToolController.kt
+++ src/main/kotlin/org/ptss/support/api/controllers/ToolController.kt
@@ -14,7 +14,7 @@ import org.ptss.support.security.Authentication
 @Path("/tools")
 @ApplicationScoped
 @Produces(MediaType.APPLICATION_JSON)
-//@Authentication(roles = [Role.PATIENT, Role.FAMILY_MEMBER, Role.HCP])
+@Authentication(roles = [Role.PATIENT, Role.FAMILY_MEMBER, Role.HCP])
 class ToolController(
     private val toolFacade: ToolFacade
     ) : IToolController {
