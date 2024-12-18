3325321ac16d0f834d1acfe590b7627218d3f015
DaanTol <686379@student.inholland.nl>
2024-12-18 21:02:45 +0100

    Removed unused imports and not needed logger

--- src/main/kotlin/org/ptss/support/infrastructure/repositories/ToolRepository.kt
+++ src/main/kotlin/org/ptss/support/infrastructure/repositories/ToolRepository.kt
@@ -1,29 +1,17 @@
 package org.ptss.support.infrastructure.repositories
 
-import com.azure.data.tables.TableClient
-import com.azure.data.tables.TableServiceClientBuilder
-import jakarta.annotation.PostConstruct
 import jakarta.enterprise.context.ApplicationScoped
-import org.ptss.support.common.exceptions.APIException
-import org.ptss.support.domain.enums.ErrorCode
-import org.ptss.support.domain.enums.MediaType
 import org.ptss.support.domain.interfaces.repositories.IToolRepository
-import org.ptss.support.domain.models.MediaInfo
 import org.ptss.support.domain.models.Tool
-import org.ptss.support.infrastructure.config.AzureStorageConfig
 import org.ptss.support.infrastructure.config.PostgreSQLConfig
 import org.ptss.support.infrastructure.persistence.entities.ToolEntity
-import org.slf4j.LoggerFactory
 import java.sql.Connection
 import java.sql.DriverManager
-import java.time.Instant
 
 @ApplicationScoped
 class ToolRepository(
     private val config: PostgreSQLConfig
 ) : IToolRepository {
-
-    private val logger = LoggerFactory.getLogger(ToolRepository::class.java)
 
     private val connection: Connection by lazy {
         DriverManager.getConnection(config.url(), config.username(), config.password())
